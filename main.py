6def487b0365fe977fde68493b506234631bbae6
SOURI-HQ <seconddan@wp.pl>
2023-11-15 22:42:06 +0100

    docs: add python docstrings documenting the code

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+"""
+This is a movie recommendation engine that utilizes the Euclidean and Pearson algorithm to recommend 5 movies to watch
+and 5 movies not to watch based off of a given list of movies listed and reviewed by a group of people on the scale
+from 0.0 to 10.0.
+
+To run this project make sure that you:
+    - download at least a 3.10 version of Python,
+    - download and install deep_translator with the following command via the terminal: pip install deep_translator,
+    - generate an api key from: https://www.omdbapi.com/apikey.aspx and use it in OMDB_API_KEY variable,
+    - launch the project from your favorite IDE with arguments specifying the user index for which the movies are being
+    recommended and the algorithm, for example:
+    "0 euclidean", "5 pearson".
+
+    The list of reviewed movies by users is fetched from data.csv file.
+
+    Project created by:
+        Kajetan Welc
+        Daniel Wirzba
+"""
+
 import csv
 import json
 import numpy as np
@@ -7,20 +27,62 @@ import sys
 
 from deep_translator import GoogleTranslator
 
-OMDB_API_KEY = "xxx"
+"""
+A variable holding the api key to omdbapi (The Open Movie Database) API that fetches movies' descriptions.
+Generate key from https://www.omdbapi.com/apikey.aspx and replace the default value to run the code.
+"""
+OMDB_API_KEY = "key"
 
 
 class Movie:
+    """
+        A class to represent a single, reviewed by a user movie.
+
+        ...
+
+        Methods
+        -------
+        __init__:
+            Method used to initialize and construct the necessary attributes for the Movie object.
+        __str__:
+            Method returning a string representation of a Movie object containing a title and API fetched description
+        __repr__:
+            Method returning a string representation of an object
+    """
     def __init__(self, title, rating):
+        """
+            Method used to initialize and construct the necessary attributes for the Movie object.
+
+            Parameters
+            ----------
+            self : instance of the class,
+            title: str,
+            rating: float
+
+            Returns
+            -------
+            None
+        """
         self.title = title
         self.rating = rating
 
     def __str__(self):
+        """
+            Method returning a string representation of a Movie object
+            consisting of a title and an API fetched description
+
+            Parameters
+            ----------
+            self : instance of the class,
+
+            Returns
+            -------
+            A String representation of a Movie object containing the title of the movie and a description of a movie
+            fetched from omdbapi API.
+        """
         try:
             description = requests.get(
-                f"https://www.omdbapi.com/?apikey={
-                    OMDB_API_KEY}&t={self.title}"
-            ).json().get("Plot")
+                f"https://www.omdbapi.com/?apikey={OMDB_API_KEY}&t={self.title}").json().get("Plot")
 
             if description == None:
                 raise Exception("Empty description")
@@ -30,6 +92,13 @@ class Movie:
         return f"{self.title} - {description}"
 
     def __repr__(self):
+        """
+        Method returning a string representation of an object
+
+        Parameters
+        ----------
+        self : instance of the class
+        """
         return self.__str__()
 
 
@@ -60,9 +129,7 @@ except:
                         [word.capitalize()
                          for word in movie_or_rating.split(' ')]
                     )
-
                     en_movie = translator.translate(capitalized_words)
-
                     data[-1].append([en_movie])
                 else:
                     data[-1][-1].append(float(movie_or_rating))
@@ -84,6 +151,21 @@ data = [
 
 
 def euclidean_score(movies1: list[Movie], movies2: list[Movie]):
+    """
+        Method calculating a similarity score based on Euclidean Distance
+
+        Parameters
+        ----------
+        movies1 : list of movies listed by a command line given user index arg,
+        movies2 : list of movies listed by a different user
+
+        Returns
+        -------
+        0 : int,
+            if there are no similar movies between a CMD given user and other user
+        an  int,
+            calculated Euclidean distance
+    """
     squared_diffs = []
 
     rating_per_title = {}
@@ -105,6 +187,21 @@ def euclidean_score(movies1: list[Movie], movies2: list[Movie]):
 
 
 def pearson_score(movies1: list[Movie], movies2: list[Movie]):
+    """
+        Method calculating a similarity score based on Pearson Correlation
+
+        Parameters
+        ----------
+        movies1 : list of movies listed by a command line given user index arg,
+        movies2 : list of movies listed by a different user
+
+        Returns
+        -------
+        0 : int,
+            if there are no similar movies between a CMD given user and other user
+        an int,
+            calculated Pearson Correlation
+    """
     rating_per_title = {}
     rating_count = 0
     user1_sum = 0
@@ -176,6 +273,20 @@ def flatten(l):
 
 
 def generate_movie_proposition(movies: list[Movie], banned_movies: set[str]):
+    """
+        Method selecting 5 movies to propose
+
+        Parameters
+        ----------
+        movies : list of movies from which a specified in NUMBER_OF_PROPOSED_MOVIES-variable number of movies is selected,
+        banned_movies : list of movies that are repeated or already recommended
+
+        Returns
+        -------
+        selected_movies : list,
+            a list containing recommended movies
+
+    """
     selected_movies = []
 
     for movie in movies:
@@ -212,7 +323,7 @@ for already_recommended_movie in recommended_movies:
     banned_movies.add(already_recommended_movie.title)
 
 
-movies_ordered_by_relevence_asc = flatten([
+movies_ordered_by_relevance_asc = flatten([
     index_and_movies[1] for index_and_movies in sorted(
         user_indexes_with_movies,
         key=lambda index_and_movies: user_scores[index_and_movies[0]]
@@ -220,6 +331,6 @@ movies_ordered_by_relevence_asc = flatten([
 ])
 
 print(f"Not recommended movies to watch for user {user_index}:")
-for movie in generate_movie_proposition(movies_ordered_by_relevence_asc, banned_movies):
+for movie in generate_movie_proposition(movies_ordered_by_relevance_asc, banned_movies):
     print(movie)
     print()
